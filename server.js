d98006257a65470d6841f372dbfbf249eaec3f7c
amlali <37159337+amlali@users.noreply.github.com>
2018-09-21 02:02:38 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const hbs = require('hbs');
+var app=express();
+
+var body={
+
+    pageTitle:'title in all pages',
+
+};
+hbs.registerPartials(__dirname +'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear() ;
+});
+app.set('view engine','hbs');
+app.use(express.static(__dirname +'/public'));
+
+app.use((req,res,next)=>{
+  res.render('newmiddel.hbs');
+});
+
+app.get('/',(req,res)=>{
+res.render('home.hbs',{
+  body,
+  welcome:'welcome to our web site'
+});
+
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{body} );
+//  res.send('<h1>hello world</h>');
+
+});
+app.listen(3000);
